966d0fdd5a121e4334214c63bd79c0d8af52afe4
Patryk Zaraziński <patrykzarazinski1a@gmail.com>
2022-11-29 20:48:27 +0100

    Little changes 1) formatting 2) added check for names longest than 15 characters

--- flask-app/static/src/index.js
+++ flask-app/static/src/index.js
@@ -1,38 +1,47 @@
 function clearInput() {
-    document.querySelector('#firstInput').value = '';
-    document.querySelector('#secondInput').value = '';
+  document.querySelector("#firstInput").value = "";
+  document.querySelector("#secondInput").value = "";
 }
 
 function main() {
-    document.addEventListener('DOMContentLoaded', function () {
-        document.querySelector('form').onsubmit = () => {
-            const firstName = document.querySelector('#firstInput').value;
-            const secondName = document.querySelector('#secondInput').value;
-            const pattern = /^[a-zA-Z][a-zA-Z0-9]+$/i;
+  document.addEventListener("DOMContentLoaded", function () {
+    document.querySelector("form").onsubmit = () => {
+      const firstName = document.querySelector("#firstInput").value;
+      const secondName = document.querySelector("#secondInput").value;
+      const pattern = /^[a-zA-Z][a-zA-Z0-9]+$/i;
 
-            console.log(firstName);
-            console.log(secondName);
+      console.log(firstName);
+      console.log(secondName);
 
-            if (firstName.length === 0 || secondName.length === 0) {
-                alert('Please provide names for both players')
-                clearInput();
-                return false;
-            }
+      if (firstName.length === 0 || secondName.length === 0) {
+        // prettier-ignore
+        alert('Please provide names for both players')
+        clearInput();
+        return false;
+      }
 
-            const expectedNameSize = 3;
-            if (firstName.length < expectedNameSize || secondName.length < expectedNameSize) {
-                alert(`Size of each name must have at least ${expectedNameSize} alphanumeric signs`)
-                clearInput();
-                return false;
-            }
+      const expectedNameSizeLow = 3;
+      const expectedNameSizeHigh = 15;
+      if (
+        firstName.length < expectedNameSizeLow ||
+        secondName.length < expectedNameSizeLow ||
+        firstName.length > expectedNameSizeHigh ||
+        secondName.length > expectedNameSizeHigh
+      ) {
+        // prettier-ignore
+        alert(`Size of each name must have at least ${expectedNameSizeLow} and no more than ${expectedNameSizeHigh} alphanumeric signs`)
+        clearInput();
+        return false;
+      }
 
-            if (!(pattern.test(firstName) && pattern.test(secondName))) {
-                alert('Each name has to be consist of alphanumeric signs and starting with letter');
-                clearInput();
-                return false;
-            }
-        }
-    });
+      if (!(pattern.test(firstName) && pattern.test(secondName))) {
+        // prettier-ignore
+        alert('Each name has to be consist of alphanumeric signs and starting with letter');
+        clearInput();
+        return false;
+      }
+    };
+  });
 }
 
-main()
+main();
